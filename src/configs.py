c9c0003023861df01b7b83f1d27462fb0194b316
Slater Victoroff <slater.r.victoroff@gmail.com>
2026-01-04 11:06:16 -0500

    Refactoring for speed and repeatability

---
+++ src/configs.py
@@ -0,0 +1,32 @@
+REFERENCES = {
+    "xxs": {
+        "grid_ranks": [1, 40, 50, 70],
+        "real_tucker_ranks": [1, 60, 60, 60],
+        "complex_tucker_ranks": [1, 45, 45, 45],
+        "conv_hidden": 84,
+    },
+    "xs": {
+        "grid_ranks": [2, 40, 50, 70],
+        "real_tucker_ranks": [2, 60, 60, 60],
+        "complex_tucker_ranks": [2, 50, 50, 50],
+        "conv_hidden": 84,
+    },
+    "small": {
+        "grid_ranks": [3, 40, 50, 70],
+        "real_tucker_ranks": [3, 80, 80, 70],
+        "complex_tucker_ranks": [3, 60, 60, 50],
+        "conv_hidden": 84,
+    },
+    "medium": {
+        "grid_ranks": [6, 40, 50, 70],
+        "real_tucker_ranks": [6, 80, 80, 70],
+        "complex_tucker_ranks": [6, 60, 70, 60],
+        "conv_hidden": 84,
+    },
+    "large": {
+        "grid_ranks": [12, 40, 50, 70],
+        "real_tucker_ranks": [12, 80, 80, 70],
+        "complex_tucker_ranks": [12, 60, 70, 60],
+        "conv_hidden": 84,
+    }
+}
